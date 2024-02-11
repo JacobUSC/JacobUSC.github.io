00a20c44e28d51a913ac9e2b5fb05089f6acd767
JacobUSC <jrr18@email.sc.edu>
2024-02-11 15:53:56 -0500

    part 4 progress

--- csce242/projects/part4/script.js
+++ csce242/projects/part4/script.js
@@ -1,29 +1,43 @@
 //css root
 const root = document.querySelector(":root");
 
-//Nav Menu
-document.getElementById("nav-button").onclick = () => {
+const showOverlay = () => {
+    root.style.setProperty("--show-overlay", "block");
+};
+
+const showNav = () => {
+    showOverlay();
     root.style.setProperty("--menu", "block");
-}
+};
 
-document.getElementById("close-nav-button").onclick = () => {
-    root.style.setProperty("--menu", "none");
-}
-
-//Signup Menu
-document.getElementById("signup-button").onclick = () => {
+const showSignup = () => {
+    showOverlay();
     root.style.setProperty("--show-signup", "block");
 };
 
-document.getElementById("exit-signup").onclick = () => {
-    root.style.setProperty("--show-signup", "none");
-};
-
-//login Menu
-document.getElementById("login-button").onclick = () => {
+const showLogin = () => {
+    showOverlay();
     root.style.setProperty("--show-login", "block");
 };
 
-document.getElementById("exit-login").onclick = () => {
+const hideAll = () => {
+    root.style.setProperty("--show-overlay", "none");
+    root.style.setProperty("--menu", "none");
+    root.style.setProperty("--show-signup", "none");
     root.style.setProperty("--show-login", "none");
-};
+}
+
+//Nav Menu
+document.getElementById("nav-button").onclick = showNav;
+document.getElementById("close-nav-button").onclick = hideAll;
+
+//Signup Menu
+document.getElementById("signup-button").onclick = showSignup;
+document.getElementById("exit-signup").onclick = hideAll;
+
+//login Menu
+document.getElementById("login-button").onclick = showLogin;
+document.getElementById("exit-login").onclick = hideAll;
+
+//background clicked
+document.getElementById("transparent-overlay").onclick = hideAll;
